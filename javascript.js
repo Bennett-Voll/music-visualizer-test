5ecb05a8f6456075f441675a9101c740162d7c68
benv.taa <benv.taa@gmail.com>
2019-01-30 21:23:03 +0100

    Added mapper, mapper randomizer and changed shape of music circle

--- javascript.js
+++ javascript.js
@@ -17,6 +17,40 @@ function shuffle(array) {
   return array;
 }
 
+/**
+ * Create an array where each element represents a map from once index to another
+ * 
+ * @param {int} length 
+ * @return {Array}
+ */
+function createIndexMapper(length) {
+    var mapper = new Array(length);
+
+    for (var i = 0; i < length; i += 1) {
+        mapper[i] = i;
+    }
+
+    mapper = shuffle(mapper);
+
+    return mapper;
+}
+
+/**
+ * Use the mapper to predictably map every element from array to an index specified in the mapper
+ * mapper and array must be of equal size
+ * 
+ * @param {Array} mapper 
+ * @param {Array} array
+ * @return {Array}
+ */
+function useIndexMapper(mapper, array) {
+    var newArray = new Array(array.length);
+
+    mapper.forEach((e, i) => newArray[e] = array[i]);
+
+    return newArray;
+}
+
 function preload() {
     sound = loadSound('assets/boc.mp3');
 }
@@ -28,6 +62,9 @@ function setup() {
     sound.amp(0.2);
 }
 
+// 1024 is the size of both the spectrum array and the waveform array
+var indexMapper = createIndexMapper(1024);
+
 function draw() {
     background(0);
 
@@ -35,30 +72,34 @@ function draw() {
     var centerY = height / 2;
     var minLength = Math.min(width, height) / 2;
 
-    var spectrum = fft.analyze();
+    var spectrum = useIndexMapper(indexMapper, fft.analyze());
     var waveform = fft.waveform();
 
     noFill();
     beginShape();
-    stroke(255, 0, 0); // waveform is red
+    stroke(196, 11, 11); // waveform is red
     strokeWeight(1);
     for (var i = 0; i < waveform.length; i += 1) {
         var waveI = i;
-        var specI = i === waveform.length - 1 ? i : i - i % 8;
+        var specI = i === waveform.length - 1 ? i : i - i % 4;
 
         var deg = map(i, 0, waveform.length, 0, Math.PI * 2);
 
-        var waveformX = map(waveform[waveI], 0, 1, 0, minLength / 2);
-        var waveformY = map(waveform[waveI], 0, 1, 0, minLength / 2);
+        var waveformX = map(waveform[waveI], -1, 1, 0, minLength / 2);
+        var waveformY = map(waveform[waveI], -1, 1, 0, minLength / 2);
 
-        var spectrumX = (i % 2 === 0 ? -1 : 1) * map(spectrum[specI], 0, 255, 0, minLength / 3);
-        var spectrumY = (i % 2 === 0 ? -1 : 1) * map(spectrum[specI], 0, 255, 0, minLength / 3);
+        var spectrumX = map(spectrum[specI], 0, 255, 0, minLength / 3);
+        var spectrumY = map(spectrum[specI], 0, 255, 0, minLength / 3);
 
         var x = centerX + (waveformX + spectrumX) * Math.cos(deg);
         var y = centerY + (waveformY + spectrumY) * Math.sin(deg);
         
         vertex(x, y);
     }
+    vertex(
+        centerX + map(waveform[0], -1, 1, 0, minLength / 2) + map(spectrum[0], 0, 255, 0, minLength / 3),
+        centerY
+    );
     endShape();
 }
 
